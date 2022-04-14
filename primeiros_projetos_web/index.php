d2d8206035579eaa3ada021575178e35bccab0d1
Andreza Santos <dehcosta.475@gmail.com>
2022-04-14 13:32:07 -0300

    envio da aula prática sobre desenvolvimento web

---
+++ primeiros_projetos_web/index.php
@@ -0,0 +1,14 @@
+<html>
+
+	<head>
+		<title>Meu primeiro site em PHP! Aeeee!</title>
+	</head>
+	
+	<body>
+		<?php
+			for ( $i = 0 ; $i < 10 ; $i++ ){
+				Print( "Linha número " . $i . "<br />");
+			}
+		?>
+	</body>
+</html>
